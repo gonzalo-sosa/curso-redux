5f595d2dd5cf0d1f4a69a8e6a7b14f0d98e5bc4e
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-18 10:12:11 -0300

    Agregado para redux developer tools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { legacy_createStore as createStore } from "redux"; // createStore deprecated
 import reducer from "./reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
