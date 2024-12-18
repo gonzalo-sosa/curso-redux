860f23ac7f8d80f3a84acbc695c867c74f30271d
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-18 16:08:38 -0300

    Uso de redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 import './App.css'
-import { actions } from "./store/bugs.js"
 import configureStore from './store/configureStore.js';
+import { bugAdded, bugAssignedToUser, bugResolved, getBugsByUser, getUnresolvedBugs } from "./store/bugs.js"
+import { projectAdded } from './store/projects.js';
+import { userAdded } from './store/users.js';
 
 function App() {
   const store = configureStore()
 
-  store.dispatch(actions.bugAdded({ description: "Bug1" }));
-  store.dispatch(actions.bugAdded({ description: "Bug2" }));
-  store.dispatch(actions.bugAdded({ description: "Bug3" }));
-  store.dispatch(actions.bugResolved({ id: 1 }));
+  store.dispatch(bugAdded({ description: "Bug 1"}))
+  store.dispatch(bugAdded({ description: "Bug 2"}))
+  store.dispatch(bugResolved({ id: 1 }))
+  store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
+  store.dispatch(projectAdded({ name: "Project 1" }))
+  store.dispatch(userAdded({ name: "Agustín"}))
+  store.dispatch(userAdded({ name: "Gonzalo" }))
+
+  console.log(
+    getBugsByUser(1)(store.getState())
+  )
 
   return (
     <>
-      <h1>Hello World! {JSON.stringify(store.getState())}</h1>
+      <h1>Redux</h1>
+
+      <h2>Bugs</h2>
+      <ul>{
+        store.getState().entities.bugs.map(bug => <li key={bug.id}>{JSON.stringify(bug)}</li>)
+      }</ul>
+      <h3>Unresolved Bugs</h3>
+      <ul>{
+        getUnresolvedBugs(store.getState()).map(bug => <li key={bug.id}>{JSON.stringify(bug)}</li>)
+      }</ul>
+      <h2>Projects</h2>
+      <ul>{
+        store.getState().entities.projects.map(project => <li key={project.id}>{JSON.stringify(project)}</li>)
+      }</ul>
+      <h2>Users</h2>
+      <ul>{
+        store.getState().entities.users.map(user => <li key={user.id}>{JSON.stringify(user)}</li>)
+      }</ul>
     </>
   )
 }
