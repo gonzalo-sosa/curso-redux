06c65b6418ea38c4bf5e26dff252ff95d9add057
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-12-18 16:07:41 -0300

    Store de proyectos

---
+++ src/store/projects.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+let lastId = 0;
+
+const projectSlice = createSlice({
+  name: "project",
+  initialState: [],
+  reducers: {
+    projectAdded: (projects, action) => {
+      projects.push({
+        id: ++lastId,
+        name: action.payload.name,
+      });
+    },
+  },
+});
+
+export const { projectAdded } = projectSlice.actions;
+export default projectSlice.reducer;
